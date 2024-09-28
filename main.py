8a5066743c2fe6a87790286120866ab1ce199bc8
GentleHUG <degrodance@yandex.ru>
2024-09-28 18:04:36 +0300

    my_process_word_func  and ep added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Response
 from matplotlib import cm
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import io
@@ -10,6 +10,8 @@ import uvicorn
 import numpy as np
 import random
 from PIL import Image, ImageDraw
+from collections import Counter
+import re
 
 with open('config.json') as config_file:
 	config = json.load(config_file)
@@ -37,13 +39,23 @@ def custom_color_func(word, font_size, position, orientation, random_state=None,
 	return rgb_color
 
 
-def process_words(string_list: StringList) -> str:
-	text = " ".join(string_list.items)
-	return text
+# // TODO Нужно реализовать функцию обработки слов)
+def precess_word(string_list: List[str]) -> Dict[str, float]:
+	return {}
 
 
-def precess_word(string_list: List[str]) -> dict[str, float]:
-	return {"lol": 1.2}
+def extract_words(sentences: List[str]) -> List[str]:
+	words = []
+	for sentence in sentences:
+		# Удаляем знаки препинания и разбиваем строку на слова
+		words.extend(re.findall(r'\b\w+\b', sentence.lower()))
+	return words
+
+
+def my_process_word_func(string_list: List[str]) -> Dict[str, float]:
+	counter = Counter(extract_words(string_list))
+	max_freq: int = counter.most_common(1)[0][1]
+	return {key: round(value / max_freq, 4) for key, value in counter.items()}
 
 
 @app.post("/generate-wordcloud/", summary="Генерация облака слов", response_description="Изображение облака слов")
@@ -54,7 +66,7 @@ async def generate_wordcloud(string_list: StringList):
 	mask = create_oval_mask(width, height)
 
 	# TODO Здесь должна быть обработка с помощью ML
-	text = process_words(string_list)
+	aggregated_words = my_process_word_func(string_list=string_list.items)
 
 	wordcloud = WordCloud(
 		width=width,
@@ -65,7 +77,7 @@ async def generate_wordcloud(string_list: StringList):
 		color_func=custom_color_func,
 		font_path=config["fort_path"],
 		prefer_horizontal=1
-	).generate(text)
+	).generate_from_frequencies(aggregated_words)
 
 	plt.figure(figsize=(10, 5))
 	plt.imshow(wordcloud, interpolation='bilinear')
@@ -78,6 +90,11 @@ async def generate_wordcloud(string_list: StringList):
 	plt.close()
 
 	return Response(content=img_byte_array.getvalue(), media_type="image/png")
+
+
+@app.post("/aggregate-words/", summary="Агрегация слов с их ценностью", response_description="Список сгенерированных слов с их ценностью")
+async def aggregate_words(string_list: StringList):
+	return {"aggregated_words": my_process_word_func(string_list.items)}
 
 
 if __name__ == "__main__":
