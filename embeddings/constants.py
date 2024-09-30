a9420ba4f84aede7e1930c2d8098a9053e71e060
Терещенко Артемий Владимирович <tereschenkoav3@zdrav.mos.ru>
2024-10-01 00:17:48 +0300

    removed unused constants.py

--- embeddings/constants.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-from typing import Dict
-
-# TODO: Надо поменять путь. 7 Гб мы никуда нормально не закинем, надо указать официальную ссылку на скачиание.
-FASTTEXT_MODEL_PATH: Path = Path("/Users/fffgson/Desktop/Coding/nuclearhack2024_local")
-TEST_WORDS_PATH: Path = Path("/Users/fffgson/Desktop/Coding/nuclearhack2024_local")
-RUBERT_MODEL_PATH: str = "cointegrated/rubert-tiny2"
