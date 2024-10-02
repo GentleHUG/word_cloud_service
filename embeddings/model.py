4d0f6d8e1e029039043ab4c5379a3277585168a9
Терещенко Артемий Владимирович <tereschenkoav3@zdrav.mos.ru>
2024-10-02 23:44:15 +0300

    updated model

--- embeddings/model.py
+++ embeddings/model.py
@@ -77,77 +77,6 @@ class WordClusterizer:
 
         return self.umap_model.fit_transform(self.embeddings)
 
-    def _get_top_words_old(
-        self,
-        reduced_embeddings: np.ndarray,
-        num_top_words: Union[int, str] = "auto",
-    ):
-        """
-        Получение топ-слов из кластеров на основе уменьшенных эмбеддингов.
-
-        :param reduced_embeddings: Уменьшенные эмбеддинги слов.
-        :param num_top_words: Количество топ-слов для извлечения (int) или 'auto' для автоматического выбора.
-        :return: Кортеж из массива топ-слов и словаря весов кластеров.
-        """
-        # Определение минимального размера кластера
-        if self.num_words < 7:
-            self.min_cluster_size = self.num_words
-        else:
-            self.min_cluster_size = max(7, int(np.log(self.num_words)))
-
-        # Кластеризация с помощью HDBSCAN
-        hdbscan_model = HDBSCAN(min_cluster_size=self.min_cluster_size)
-        self.labels = hdbscan_model.fit_predict(reduced_embeddings)
-
-        # Объединение эмбеддингов, лейблов и слов
-        together = np.concatenate(
-            (self.labels.reshape(-1, 1), self.word_list.reshape(-1, 1)), axis=1
-        )
-
-        # Отбрасываем -1 (непринадлежащие кластерам)
-        self.together = together[together[:, 0] != "-1"]
-
-        # Подсчет количества вхождений в каждый кластер
-        cluster_counts = Counter(together[:, 0])
-
-        # Получение самых больших кластеров
-        top_clusters = cluster_counts.most_common(max(1, len(cluster_counts) // 2))
-
-        # Общее количество элементов в топ-кластерах
-        total_elements = sum(count for _, count in top_clusters)
-
-        # Получение слов из самых больших кластеров
-        top_clusters_words = []
-        weights = {}
-        for cluster_id, count in top_clusters:
-            top_clusters_words.extend(together[together[:, 0] == str(cluster_id)][:, 1])
-            weights[int(cluster_id)] = (
-                count / total_elements if total_elements > 0 else 0
-            )
-
-        word_counts = Counter(top_clusters_words)
-
-        # Определение количества топ-слов
-        if num_top_words == "auto":
-            self.num_top_words = max(1, len(word_counts) // 2)
-        else:
-            if num_top_words > self.num_words:
-                self.num_top_words = self.num_words
-            else:
-                self.num_top_words = num_top_words
-
-        # Получение самых частых слов
-        _top_words = word_counts.most_common(max(1, self.num_top_words))
-
-        top_words = []
-        for word, _ in _top_words:
-            top_words.append(word)
-
-        self.weights = weights
-        self.top_words = np.array(top_words)
-
-        return np.array(top_words), weights
-
     def _cosine_distance(self, a: np.ndarray, b: np.ndarray) -> float:
         """
         Вычисление косинусного расстояния между двумя векторами.
@@ -268,34 +197,6 @@ class WordClusterizer:
         # self.top_words = np.array(top_words)
 
         return clusters_data
-
-    def forward_old(
-        self, words: np.ndarray, num_top_words: Union[int, str] = "auto"
-    ) -> List[TopCluster]:
-        """
-        Основной метод для обработки списка слов, кластеризации и получения топ-слов.
-
-        :param words: Numpy массив слов для обработки.
-        :param num_top_words: Количество топ-слов для извлечения (int) или 'auto' для автоматического выбора.
-        :return: Объект Topwords, содержащий список топ-слов, веса и косинусные расстояния.
-        """
-        # Кластеризация слов
-        logging.info("Clusterizing words.")
-        clustered_words = self._cluster_words(words)
-        # Получение топ-слов и их весов
-        logging.info("Getting top words.")
-        top_words, weights = self._get_top_words(
-            clustered_words, num_top_words=num_top_words
-        )
-        # Получение косинусных расстояний
-        logging.info("Getting cosines..")
-        cosines = self._get_cosines(top_words)
-        result = []
-        return TopCluster(
-            words_list=top_words.astype(np.str_),
-            weights=weights,
-            cosines=cosines,
-        )
 
     def forward(
         self, words: np.ndarray, num_top_words: Union[int, str] = "auto"
